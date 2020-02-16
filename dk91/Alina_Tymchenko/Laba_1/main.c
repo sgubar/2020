62bc068846ed7fccd301175bd5497a609014c4b0
alina-alya <55531135+alina-alya@users.noreply.github.com>
2020-02-16 21:41:20 +0200

    There is my c file

---
+++ dk91/Alina_Tymchenko/Laba_1/main.c
@@ -0,0 +1,28 @@
+#include "555.h"
+
+double main()
+{
+  double A, B, C, D;
+  char *buff;
+  buff = (char*)malloc(sizeof(char) * 128); // char buff[128];
+
+  printf("Print A: \n");
+  fgets(buff, 128, stdin);
+  A = protect_number(buff, strlen(buff));
+  free(buff);
+
+  printf("Print B: \n");
+  fgets(buff, 128, stdin);
+  B = protect_number(buff, strlen(buff));
+  free(buff);
+
+  printf("Print C: \n");
+  fgets(buff, 128, stdin);
+  C = protect_number(buff, strlen(buff));
+  free(buff);
+
+  double result = drob(A, B, C) + Sum(B);
+  printf("Q = %lf\n", result);
+
+  return 0;
+}
