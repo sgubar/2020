4630436988f9c15a1586233fb3e28a4c8d9b2fca
Amulvaro <55878542+Amulvaro@users.noreply.github.com>
2020-02-16 21:41:25 +0200

    Create hidder.h

---
+++ dk91/Aleshko/Lab1/hidder.h
@@ -0,0 +1,9 @@
+#ifndef QWE_H_INCLUDED
+#define QWE_H_INCLUDED
+
+double power(int base, int exp);
+double mod(int a, int b, int c);
+double sum(int finish, int start);
+double fac(int base);
+
+#endif // QWE_H_INCLUDED
