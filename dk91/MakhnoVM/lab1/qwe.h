5aa56526a5b828d757d648366fc783f3540658df
Lixxxymaster <55249263+Lixxxymaster@users.noreply.github.com>
2020-02-16 18:57:07 +0200

    Add files via upload

---
+++ dk91/MakhnoVM/lab1/qwe.h
@@ -0,0 +1,8 @@
+#ifndef QWE_H_INCLUDED
+#define QWE_H_INCLUDED
+
+double power(int base, int exp);
+double mod(int a, int b, int c);
+double sum(int base, int finish, int start);
+
+#endif // QWE_H_INCLUDED
