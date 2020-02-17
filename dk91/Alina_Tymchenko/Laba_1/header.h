4917272abfb72ce9e4cde0ae441e0340bde10cbf
alina-alya <55531135+alina-alya@users.noreply.github.com>
2020-02-17 19:56:37 +0200

    there is my header file

---
+++ dk91/Alina_Tymchenko/Laba_1/header.h
@@ -0,0 +1,10 @@
+#ifndef HEADER_H_INCLUDED
+#define HEADER_H_INCLUDED
+
+double drob(double A, double B, double C);
+int fact(int D);
+int Sum(int B);
+float protect_number (char *buff, int len);
+
+#endif // HEADER_H_INCLUDED
+
