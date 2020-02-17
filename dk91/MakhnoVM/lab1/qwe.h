70c0041b3b785d9da13022e4b7875861b5ee10a6
Lixxxymaster <55249263+Lixxxymaster@users.noreply.github.com>
2020-02-17 20:50:59 +0200

    Delete qwe.h

--- dk91/MakhnoVM/lab1/qwe.h
+++
@@ -1,8 +0,0 @@
-#ifndef QWE_H_INCLUDED
-#define QWE_H_INCLUDED
-
-double power(int base, int exp);
-double mod(int a, int b, int c);
-double sum(int base, int finish, int start);
-
-#endif // QWE_H_INCLUDED
