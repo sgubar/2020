0db6064c223dce7166e6ec5a895d988a7ed97f55
KTymchenko01 <55530875+KTymchenko01@users.noreply.github.com>
2020-02-17 20:02:55 +0200

    There is function solution

--- dk91/ Тимченко_Катерина/ лр1/main.c
+++ dk91/ Тимченко_Катерина/ лр1/main.c
@@ -1,4 +1,7 @@
-#include "header.h"
+#include "hyder.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
 
 double main(int argc, char const *argv[])
 {
@@ -25,10 +28,59 @@ double main(int argc, char const *argv[])
   fgets(buff, 128, stdin);
   D = protect_number(buff, strlen(buff));
   free(buff);
-  
+
   double result = drob(A, B, C) * Sum(A);
   printf("Your result is: %0.2lf\n", result);
 
+
+  return 0;
+}
+float drob(double A, double B, double C)
+{
+  double result = 0;
+  if(C != 0)
+  {
+    result = (abs(A * C - B)) / (pow(C, 3));
+    return result;
+  }
+  else
+    printf("Error\n");
+}
+
+int Sum(int A)
+{
+  int result = 0;
+  for(int D = 0; D <= A; D++)
+    result += pow(2, D);
+  return result;
+}
+
+float protect_number (char *buff, int len)
+{
+  int prot = 0, point = 0, znak = 0;
+  float number;
+  while(prot != len || znak > 1 || point > 1)
+  {
+    prot = 1;
+    point = 0;
+    znak = 0;
+
+    for(int i = 0; i < len; i++)
+    {
+      if(buff[i] >= '0' && buff[i] <= '9') prot++;
+      else if(buff[i] == '.'){point++; prot++;}
+      else if(buff[i] == '-'){znak++; prot++;}
+    }
+    //printf("%d %d\n", prot, len);
+        if(prot != len)
+    {
+      printf("It isn't number! Try again\n");;
+      fgets(buff, 128, stdin);
+    }
+  }
+  number = atof(buff);
+  return number;
+}
 
   return 0;
 }
