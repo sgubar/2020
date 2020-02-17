68397396fca1606347a85ee90267596b50d7ac9b
KTymchenko01 <55530875+KTymchenko01@users.noreply.github.com>
2020-02-17 19:58:40 +0200

    There is description of function

--- dk91/ Тимченко_Катерина/ лр1/function.h
+++ dk91/ Тимченко_Катерина/ лр1/function.h
@@ -1,50 +1,8 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <math.h>
+#ifndef HYDER_H_INCLUDED
+#define HYDER_H_INCLUDED
 
-float drob(double A, double B, double C)
-{
-  double result = 0;
-  if(C != 0)
-  {
-    result = (abs(A * C - B)) / (pow(C, 3));
-    return result;
-  }
-  else
-    printf("Error\n");
-}
+float drob(double A, double B, double C);
+int Sum(int A);
+float protect_number (char *buff, int len);
 
-int Sum(int A)
-{
-  int result = 0;
-  for(int D = 0; D <= A; D++)
-    result += pow(2, D);
-  return result;
-}
-
-float protect_number (char *buff, int len)
-{
-  int prot = 0, point = 0, znak = 0;
-  float number;
-  while(prot != len || znak > 1 || point > 1)
-  {
-    prot = 1;
-    point = 0;
-    znak = 0;
-    
-    for(int i = 0; i < len; i++)
-    {
-      if(buff[i] >= '0' && buff[i] <= '9') prot++;
-      else if(buff[i] == '.'){point++; prot++;}
-      else if(buff[i] == '-'){znak++; prot++;}
-    }
-    //printf("%d %d\n", prot, len);
-        if(prot != len)
-    {
-      printf("It isn't number! Try again\n");;
-      fgets(buff, 128, stdin);
-    } 
-  }
-  number = atof(buff);
-  return number;
-}
+#endif // HYDER_H_INCLUDED
