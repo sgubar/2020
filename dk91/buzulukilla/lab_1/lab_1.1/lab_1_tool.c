4cda45977ad1995962b30f860514e79ff754dd34
buzulukilla <55531628+buzulukilla@users.noreply.github.com>
2020-02-18 12:31:46 +0300

    Create lab_1_tool.c

---
+++ dk91/buzulukilla/lab_1/lab_1.1/lab_1_tool.c
@@ -0,0 +1,51 @@
+#include "lab_1_tool.h"
+
+int devisible_test(int A,int B,int C){
+  
+  int Result_DEVIS=A*C-B;
+    if (Result_DEVIS<0)
+    {
+      int Result_DEVIS=Result_DEVIS*(-1);
+      }
+      return Result_DEVIS;
+}
+  
+ int exponentiation_test(int C){
+    int C1=C;
+      for (int i=0; i!=3; i++)
+    {
+    C=C*C1;
+    }
+    int Result_EXP=C;
+    
+    return Result_EXP;
+    }
+    
+    int division_test(int Result_DEVIS, int Result_EXP)
+    { 
+      return Result_DEVIS/Result_EXP;
+     }
+     
+	 
+	 
+	 int D=0;
+     int Result_EXP2=0;
+     
+	 
+	 
+	 int SUMM_test(int A,int D)
+     {
+     
+     
+     for (; D!=A; D++)
+     {
+     int Exp1=2;
+      for (int i=0; i!=D; i++)
+      {
+      Exp1=Exp1*2;
+        }
+        int Result_EXP2=Result_EXP2+Exp1;
+      }
+      
+      return Result_EXP2;
+      }
