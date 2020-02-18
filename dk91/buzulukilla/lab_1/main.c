85590a3a685a05c1fef663035424dfac6e55b221
buzulukilla <55531628+buzulukilla@users.noreply.github.com>
2020-02-18 11:38:51 +0300

    Update main.c

--- dk91/buzulukilla/lab_1/main.c
+++ dk91/buzulukilla/lab_1/main.c
@@ -5,19 +5,21 @@ int main(int argc, char** argv) {
 	int A,B,C,DEV_FIN,SUMM,RESULT;
 	double devis,exponated;
 	
-	double devis=devisible_test(int A,B,C);
+	devis=devisible_test(A,B,C);
 	
-	double exponated= exponentiation_test(C);
+	exponated= exponentiation_test(C);
 	
-	int DEV_FIN = division_test(int devis,exponated);
+DEV_FIN = division_test(devis,exponated);
+
+int D=0;
 	
-	int SUMM=SUMM_test(int A, D=0);
+SUMM=SUMM_test(A,D);
 	
-	int RESULT=0;
+RESULT=0;
 	
 	RESULT = DEV_FIN*SUMM;
 	
-	printf("the result is %d\n",RESULT)
+	printf("the result is %d\n",RESULT);
 	
 	return 0;
 }
