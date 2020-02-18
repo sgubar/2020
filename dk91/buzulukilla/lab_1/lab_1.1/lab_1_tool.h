1aa8f2bd4dfcb9c0027008586ad0ac809982c907
buzulukilla <55531628+buzulukilla@users.noreply.github.com>
2020-02-18 12:32:25 +0300

    Create lab_1_tool.h

---
+++ dk91/buzulukilla/lab_1/lab_1.1/lab_1_tool.h
@@ -0,0 +1,12 @@
+#ifndef lab_1_tool_h
+#define lab_1_tool_h
+
+#include <stdio.h>
+#include <iostream>
+
+int devisible_test(int A,int B,int C);
+int exponentiation_test(int C);
+int division_test(int devisible_test, int exponentiation);
+int SUMM_test(int A, int D);
+
+#endif
