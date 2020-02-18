201b03b46b3c1150cb6d68e4f63b9eebdfc8f928
buzulukilla <55531628+buzulukilla@users.noreply.github.com>
2020-02-18 11:34:01 +0300

    Update lab_1_tool.c

--- dk91/buzulukilla/lab_1/lab_1_tool.c
+++ dk91/buzulukilla/lab_1/lab_1_tool.c
@@ -1,11 +1,11 @@
 #include "lab_1_tool.h"
 
-int devisible_test(int A,B,C){
+int devisible_test(int A,int B,int C){
   
   int Result_DEVIS=A*C-B;
     if (Result_DEVIS<0)
     {
-      Result_DEV=Result_DEVIS*(-1);
+      int Result_DEV=Result_DEVIS*(-1);
       }
       return Result_DEVIS;
 }
@@ -14,31 +14,33 @@ int devisible_test(int A,B,C){
     int C1=C;
       for (int i=0; i==3; i++)
     {
-    C=C*C1
+    C=C*C1;
     }
     int Result_EXP=C;
     
     return Result_EXP;
     }
     
-    int division_test(int Result_DEVIS, Result_EXP)
+    int division_test(int Result_DEVIS, int Result_EXP)
     {
-      Result_DEV=Result_DEVIS/Result_EXP;
+      int Result_DEV=Result_DEVIS/Result_EXP;
       
       return Result_DEV;
      }
+     int D=0;
      
-     int SUMM_test(int A, D=0)
+     int SUMM_test(int A,int D)
      {
+     int Result_EXP2=0;
      
      for (int D=0; D==A; D++)
      {
-     Exp1=2
+     int Exp1=2;
       for (int i=0; i==D; i++)
       {
       Exp1=Exp1*2;
         }
-        Result_EXP2=Result_EXP2+Exp1;
+        int Result_EXP2=Result_EXP2+Exp1;
       }
       
       return Result_EXP2;
