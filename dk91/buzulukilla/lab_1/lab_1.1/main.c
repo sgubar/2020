1baed3bd4d7bbc3a9a2fe1e728426b9607a0e98c
buzulukilla <55531628+buzulukilla@users.noreply.github.com>
2020-02-18 12:32:46 +0300

    Create main.c

---
+++ dk91/buzulukilla/lab_1/lab_1.1/main.c
@@ -0,0 +1,28 @@
+#include<stdio.h>
+#include "lab_1_tool.h"
+int main(int argc, char** argv) {
+	
+	float A,B,C,DEV_FIN,SUMM,RESULT;
+	double devis,exponated;
+	
+	A=10;
+	B=10;
+	C=10;
+	devis=devisible_test(A,B,C);
+	
+	exponated= exponentiation_test(C);
+	
+DEV_FIN = division_test(devis,exponated);
+
+int D=0;
+	
+SUMM=SUMM_test(A,D);
+	
+RESULT=0;
+	
+	RESULT = DEV_FIN*SUMM;
+	
+	printf("the result is %d\n",RESULT);
+	
+	return 0;
+}
