6b23ea7edc0fe9a081fb8e9f79bb40d492192900
Juan-Caiza06 <55527656+Juan-Caiza06@users.noreply.github.com>
2020-02-18 11:20:27 +0200

    Update main.c

--- dk91/JuanCAIZA/LAB1/main.c
+++ dk91/JuanCAIZA/LAB1/main.c
@@ -9,7 +9,7 @@ int main()
     int a, b, c, d = 0;
     scanf("%d %d %d", &a, &b , &c);
     double qwe;
-    qwe = ((a * b)/(b + power(c, 2)))* sum(b, d) ;
+    qwe = ((a * b)/(b + step(c, 2)))* sum(b, d) ;
     printf(" qwe%f", qwe);
     return 0;
 }
