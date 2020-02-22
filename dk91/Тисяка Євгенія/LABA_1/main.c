cc4118c5ea743fb3223f6371cd049b03f2d6aa9c
Тисяк Євгеній <55532252+LAMPA23@users.noreply.github.com>
2020-02-22 18:57:42 +0200

    Update main.c

--- dk91/Тисяка Євгенія/LABA_1/main.c
+++ dk91/Тисяка Євгенія/LABA_1/main.c
@@ -1,16 +1,23 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include"hhh.h"
-#include"ccc.c"
+#include"DK91.h"
+#include"DK91.c"
 int main ()
 {
-  float A;
-  float B;
-  float C;
-  float D;
-  printf("{A, B, C, D}-Z, A>=0\n");
-  scanf("%f %f %f %f", &A, &B, &C, &D);
-  printf("%f %f %f %f \n", A, B, C, D);
+  float A, B, C, D;
+  printf("Enter the value of variable A(A is a natural number and greater than zero):");
+  A =inputVariables(A);
+   printf("A=%f\n", A);
+  printf("Enter the value of variable B(B is a natural number):");
+  B =inputVariables(B);
+   printf("B=%f\n", B);
+   printf("Enter the value of variable C(C is a natural number):");
+  C =inputVariables(C);
+   printf("C=%f\n", C);
+   printf("Enter the value of variable D(D is a natural number):");
+  D =inputVariables(D);
+   printf("D=%f\n", D);
+
   if(A<0 || 100-B-C==0)
   {
     printf("error");
@@ -19,6 +26,6 @@ int main ()
    float v1=d(A, B, C);
    float v2=s(A, B, C, D);
    float Q = q(v1, v2);
-   printf("%f %f %f",v1, v2, Q);
+  printf("v1=%f\nv2=%f\nQ=%f",v1, v2, Q);
 
 }
