76be32e32da441cd4b25c7a0e65b3ebd907b5f05
Тисяк Євгеній <55532252+LAMPA23@users.noreply.github.com>
2020-02-22 19:00:59 +0200

    DK91.h

---
+++ dk91/Тисяка Євгенія/LABA_1/DK91.h
@@ -0,0 +1,8 @@
+#ifndef DK91_H_INCLUDED
+#define DK91_H_INCLUDED
+float  d(float A, float B, float C);
+float s(float A,float B, float C, float D);
+float q(float d, float s);
+float inputVariables(float B);
+
+#endif // DK91_H_INCLUDED
