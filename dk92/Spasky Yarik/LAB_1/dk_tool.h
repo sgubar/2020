17e3170b4224bae876a8c56c90067db8b226d1b3
Jariksp <55439414+Jariksp@users.noreply.github.com>
2020-02-23 03:35:04 +0200

    Update dk_tool.h

--- dk92/Spasky Yarik/LAB_1/dk_tool.h
+++ dk92/Spasky Yarik/LAB_1/dk_tool.h
@@ -1,1 +1,13 @@
 
+#ifndef DK_TOOL_H
+#define DK_TOOL_H
+
+#include <stdlib.h>
+#include <stdio.h>
+
+int dk_factorial(int num_A);
+double dk_mnogennya( float num_A );
+double dk_suma( double result_1,float num_B);
+double dk_dilennya(double result,double result_2);
+
+#endif
