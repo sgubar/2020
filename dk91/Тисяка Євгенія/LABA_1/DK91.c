5a588558ed282a116efea2d0a467a6cff3ed1364
Тисяк Євгеній <55532252+LAMPA23@users.noreply.github.com>
2020-02-24 12:53:21 +0200

    Update DK91.c

--- dk91/Тисяка Євгенія/LABA_1/DK91.c
+++ dk91/Тисяка Євгенія/LABA_1/DK91.c
@@ -1,3 +1,30 @@
+float power2(float a, float b)
+{
+   int x=1;
+   int i;
+   int j;
+   int step;
+    if((a+b)>=0);
+    {
+       step=a+b;
+
+      for(j=0; j<step; j++)
+      {
+        x=x*2;
+      }
+     return x;
+    }
+    if(a+b<0)
+    {
+       step=-1*(a+b);
+      for(j=0; j<step; j++)
+      {
+        x=x*2;
+      }
+      x=1/x;
+      return x;
+    }
+}
 float  d(float A, float B, float C)
 {
 
@@ -10,7 +37,7 @@ float  d(float A, float B, float C)
     {
 
      printf("error:you cannot divide by zero");
-      return 0;
+     return 0;
   }
 
 
@@ -20,31 +47,13 @@ float s(float A,float B, float C, float D)
 float s=0;
 int i;
 int j;
-int step;
+
 float x;
   for(i=0; i<=A; i++)
   {
 
-     x=1;
-    if((i+D)>=0);
-    {
-       step=i+D;
-
-      for(j=0; j<step; j++)
-      {
-        x=x*2;
-      }
-
-    }
-    if(i+D<0)
-    {
-       step=-1*(i+D);
-      for(j=0; j<step; j++)
-      {
-        x=x*2;
-      }
-      x=1/x;
-    }
+    x=1;
+    x = power2(i, D);
     s=s+x;
   }
   return s;
