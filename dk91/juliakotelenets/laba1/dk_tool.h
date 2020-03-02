8de7e1a8a3d3c8ae182945e227d367ddeb6e9c68
kotelents <55530813+kotelents@users.noreply.github.com>
2020-03-02 22:18:41 +0200

    Create dk_tool.h

---
+++ dk91/juliakotelenets/laba1/dk_tool.h
@@ -0,0 +1,14 @@
+//  lab1
+//
+//  Created by Kotelenets Julia on 02/03/20.
+#ifndef dk_tool.h
+#define dk_tool.h
+
+#include <stdio.h>
+#include <stdlib.h>
+
+void Factorial(int A, int *factorial);
+void Modul(int A, int B, int *ModuledZnamenatel, int *Znamenatel);
+
+void Result(int factorial, int *Znamenatel, int *ModuledZnamenatel, float *result);
+#endif
