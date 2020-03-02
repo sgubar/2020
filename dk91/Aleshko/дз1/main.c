836ed48af916cab75e7905556dd1198b2b36dc30
Amulvaro <55878542+Amulvaro@users.noreply.github.com>
2020-03-02 18:39:49 +0200

    Create main.c

---
+++ dk91/Aleshko/дз1/main.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+int main()
+{
+    int matrix[3][5];
+    int i = 0, j = 0;
+    int d;
+
+
+    for( i = 0; i < 3; i++){
+        for( j = 0; j < 5; j++){
+            matrix[i][j] = rand() % 55;
+      printf("%d\t", matrix[i][j]);
+        } printf("\n");
+    }
+
+
+    printf("vvedi chislo = ");
+    scanf("%d", &d);
+    int b=3; int c=5;
+    Umnozenie(matrix, b, c, d);
+    
+}
