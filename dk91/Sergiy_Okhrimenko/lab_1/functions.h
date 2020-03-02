c007c3a9c1e0ff0fb956d0689c9fa8e3792aad95
Xsergiy27 <55531698+Xsergiy27@users.noreply.github.com>
2020-03-03 00:03:53 +0200

    Create functions.h

---
+++ dk91/Sergiy_Okhrimenko/lab_1/functions.h
@@ -0,0 +1,12 @@
+#ifndef FUNCTIONS_H_INCLUDED
+#define FUNCTIONS_H_INCLUDED
+
+#include <stdio.h>
+
+int factorial (int value);
+int summ (int from, int to, int value);
+float divide_func ( float first, float second);
+
+
+
+#endif // FUNCTIONS_H_INCLUDED
