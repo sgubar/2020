3901bdc74105495a5501924fa20493930221516b
buzulukilla <55531628+buzulukilla@users.noreply.github.com>
2020-03-03 11:47:58 +0300

    Add files via upload

---
+++ dk91/buzulukilla/papka/main.c
@@ -0,0 +1,41 @@
+#include "lab_1_tool.h"
+int main(int argc, char** argv) {
+	
+	int A,B,C,D;
+	double devis,exponated;
+	float SUMM,RESULT;
+	
+		
+printf("enter next variable's\n");
+
+	printf("\nA=");
+	scanf("%i",&A);
+	
+	
+	
+		printf("\nB=");
+		scanf("%i",&B);
+			
+			
+			
+			printf("\nC=");
+			scanf("%i",&C);	
+			
+			
+											
+	devis=devisible_test(A,B,C);
+	exponated= exponentiation_test(C);
+										
+	float DEV_FIN = division_test(devis,exponated);
+
+	
+	SUMM=SUMM_test(A);
+
+	RESULT=0;
+	
+	RESULT = DEV_FIN*SUMM;
+	
+	printf("the result is %.3f\n",RESULT);
+	
+	return 0;
+}
