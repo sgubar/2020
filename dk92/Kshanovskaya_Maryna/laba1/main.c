fff22025450c21e76d464be73f83a76e21b74390
kshanovska <marinakshanovska@gmail.com>
2020-03-03 19:14:45 +0200

    main.c

--- dk92/Kshanovskaya_Maryna/laba1/main.c
+++ dk92/Kshanovskaya_Maryna/laba1/main.c
@@ -1,1 +1,23 @@
+#include <stdio.h>
+#include "tool.h"
+ 
+int main(int argc, char *argv[])
+{
+    printf("vvedite a\n");
+    int a;
+    scanf("%i", &a);
+     
+    printf("vvedite b\n");
+    int b;
+    scanf("%i", &b);
 
+    printf("vvedite c\n");
+    int c;
+    scanf("%i", &c);
+
+    int result;
+    result = formula(a, b, c);
+    printf ("result = %i\n", result);
+    
+    return 0;
+}
