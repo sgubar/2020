8889c5d66ee45dee14edc696d63cb57fe7e0ee31
buzulukilla <55531628+buzulukilla@users.noreply.github.com>
2020-03-18 16:23:34 +0300

    Add files via upload

---
+++ dk91/buzulukilla/lab_1/lab_1_FINAL/main.cpp
@@ -0,0 +1,64 @@
+#include<stdio.h>
+#include "lab_1_tool.h"
+int main(int argc, char** argv) {
+	
+	int A,B,C,D;
+	double devis,exponated,zmin[3];
+	float SUMM,RESULT;
+	
+      
+	  	
+	  printf("enter next variables(no letters are allowed)");		
+			  
+	printf("\nA=");
+     
+    int DEF=defence (&A);
+    
+    if(DEF==0)
+    
+    {
+	return 0;
+     } 
+     else{printf("\nB=");
+    
+	  		 int DEF=defence (&B);
+		  
+		  if(DEF==0)
+		  
+		  {return 0;
+			  }	
+			  else{printf("\nC=");
+	  		
+			  
+	  	
+		  int DEF=defence (&C);
+		  
+		  if(DEF==0 || C==0)
+		  
+		  {return 0;
+			  }	
+			  else{
+			  }
+	  	
+			  }
+	  	
+			  }
+	  	
+			  	
+											
+	devis=devisible_test(A,B,C);
+	exponated= exponentiation_test(C);
+										
+	float DEV_FIN = division_test(devis,exponated);
+
+	
+	SUMM=SUMM_test(A);
+
+	RESULT=0;
+	
+	RESULT = DEV_FIN*SUMM;
+	
+	printf("the result is %.3f\n",RESULT);
+	
+	return 0;
+}
