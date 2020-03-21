49f3e6e1618d48b9b511f811dd4a299f8714d524
Jariksp <55439414+Jariksp@users.noreply.github.com>
2020-03-21 18:53:35 +0200

    Create dk_tool.h

---
+++ dk92/Spasky Yarik/LABA 2/dk_tool.h
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+void BubbleSort ( char buffer[] , long length);
+void InsertionSort ( char buffer[] , long length);
+void SelectionSort(long length , char buffer[]);
+void SortSelect (int type_of_sort, char buffer[], long length);
+void inPut( long length, char buffer[]);
+void Open();
