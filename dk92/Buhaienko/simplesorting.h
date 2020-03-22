767eb88eb6e284135beb47060523c11ee2254c4b
Emit-Remmus <60665171+Emit-Remmus@users.noreply.github.com>
2020-03-22 14:50:11 +0200

    Delete simplesorting.h

--- dk92/Buhaienko/simplesorting.h
+++
@@ -1,8 +0,0 @@
-#ifndef SIMPLESORTING_H_INCLUDED
-#define SIMPLESORTING_H_INCLUDED
-#include "simplesorting.c"
-void bubbule(char Array[], int Size);
-void insertion(char Array[], int Size);
-void selection(char Array[], int Size);
-
-#endif // SIMPLESORTING_H_INCLUDED
