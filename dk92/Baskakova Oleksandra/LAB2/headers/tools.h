3f05efd36d1e51ebe6dda67f14b51aa4660bd36e
DeerAkira <54910051+DeerAkira@users.noreply.github.com>
2020-03-22 23:48:54 +0200

    Delete tools.h

--- dk92/Baskakova Oleksandra/LAB2/headers/tools.h
+++
@@ -1,9 +0,0 @@
-#include <stdlib.h>
-#include <stdio.h>
-#include "sortResult.c"
-
-
-int *fillArray(int size);
-int *copyArray(int *array, int size);
-void swap(int *arr, int firtsIndex, int secondIndex);
-void fprintResult(FILE *file, char label[], SortResult result, int size);
