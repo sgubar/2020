ce394b2fca2aec020869326aa28f1e8e21edbf81
Emit-Remmus <60665171+Emit-Remmus@users.noreply.github.com>
2020-03-22 14:39:17 +0200

    Add files via upload

---
+++ dk92/Buhaienko/simplesorting.h
@@ -0,0 +1,8 @@
+#ifndef SIMPLESORTING_H_INCLUDED
+#define SIMPLESORTING_H_INCLUDED
+#include "simplesorting.c"
+void bubbule(char Array[], int Size);
+void insertion(char Array[], int Size);
+void selection(char Array[], int Size);
+
+#endif // SIMPLESORTING_H_INCLUDED
