6cd69eb35f65e079d42a1928e5bf8afe34d8decc
Gall8965 <55531309+Gall8965@users.noreply.github.com>
2020-03-22 18:03:32 +0200

    Update main.c

--- dk91/KirMisha/LAB1/main.c
+++ dk91/KirMisha/LAB1/main.c
@@ -4,10 +4,18 @@
 
 int main()
 {
-    int a, b, c, d = 0;
-    scanf("%d %d %d", &a, &b , &c);
+   int a, b, c, h, d = 0;
+   do{
+   printf("a=");
+        scanf("%d", &a);
+   }while(a <= 0);
+    do{
+        printf("b,c=");
+        scanf("%d %d", &b, &c);
+        h = b + c;
+    }while(h == 100);
     double f = a+b+c;
-    double g = 100-b-c;
+    double g = 100-h;
     double rezultat;
     rezultat =  (f / g) + sum(d, a) ;
     printf("%f", rezultat);
