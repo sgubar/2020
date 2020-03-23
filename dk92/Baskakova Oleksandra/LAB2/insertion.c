8ddc46c1e30933730c3f95e2801aed0eab2ec0b4
DeerAkira <54910051+DeerAkira@users.noreply.github.com>
2020-03-23 14:36:24 +0200

    Update insertion.c
    Уже не нужно

--- dk92/Baskakova Oleksandra/LAB2/insertion.c
+++ dk92/Baskakova Oleksandra/LAB2/insertion.c
@@ -3,13 +3,11 @@
 SortResult insertionSort(int *array, int size)
 {
     int *arr = copyArray(array, size);
-    int iterations = 0;
     time_t t0 = time(0);
     for (int i = 1; i < size; i++)
     {
         for (int j = i; j > 0 && arr[j - 1] > arr[j]; j--)
         {
-            iterations++;
             swap(arr, j, j - 1);
         }
     }
