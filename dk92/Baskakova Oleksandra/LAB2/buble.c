e62e655c392e9214b27311f34c4d3be5b8818bc3
DeerAkira <54910051+DeerAkira@users.noreply.github.com>
2020-03-23 17:25:19 +0200

    Update buble.c

--- dk92/Baskakova Oleksandra/LAB2/buble.c
+++ dk92/Baskakova Oleksandra/LAB2/buble.c
@@ -3,25 +3,30 @@
 SortResult bubleSort(int *array, int size)
 {
     int *arr = copyArray(array, size);
-    int swaps;
-    time_t t0 = time(0);
-    do
-    {
-        swaps = 0;
-        for (int i = 0; i < size - 1; i++)
-        {
-            if (arr[i] > arr[i + 1])
-            {
-                swap(arr, i, i + 1);
-                swaps++;
-            }
-        }
-    } while (swaps != 0);
 
+    time_t t0 = time(0);
+    buble(arr, size);
     time_t t1 = time(0);
 
     SortResult result;
     result.ellapsedTime = difftime(t1, t0);
     result.array = arr;
     return result;
+}
+
+void buble(int *array, int size)
+{
+    int swaps;
+    do
+    {
+        swaps = 0;
+        for (int i = 0; i < size - 1; i++)
+        {
+            if (array[i] > array[i + 1])
+            {
+                swap(array, i, i + 1);
+                swaps++;
+            }
+        }
+    } while (swaps != 0);
 }
