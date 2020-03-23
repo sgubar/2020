fefc040dc77b0f145b651b788972f6a01542da7c
kotelents <55530813+kotelents@users.noreply.github.com>
2020-03-23 20:15:42 +0200

    Create dk_tool.c

---
+++ dk91/juliakotelenets/dz1/dk_tool.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+int *reversal(int *arr,int len)
+{
+	int swap=0;
+	for (int i = 0; i < len/2; i++)
+	{
+		swap = arr[i];
+		arr[i] = arr[len - i-1];
+		arr[len - i-1] = swap;
+	}
+	return arr;
+}
