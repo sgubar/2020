34278c1bfb369135afe6320d7a4f858bce347cd7
Dmytro <55019079+dmk0704@users.noreply.github.com>
2020-03-24 22:49:09 +0200

    Create dk_tool.h

---
+++ dk92/Komarenko Dmytro/Lab1/dk_tool.h
@@ -0,0 +1,11 @@
+#ifndef DK_TOOL_H_INCLUDED
+#define DK_TOOL_H_INCLUDED
+
+int factorial(int n);
+int module(int n);
+int multiplication (int x,int y);
+double division (double x,double y);
+int sum (int x,int y);
+double q(double a, double b);
+int valid (int a,char d,int c);
+#endif // DK_TOOL_H_INCLUDED
