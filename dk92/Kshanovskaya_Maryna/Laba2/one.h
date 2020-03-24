e2a8ef53c8aa761784e4a010c1e0fc151b88a88a
kshanovska <marinakshanovska@gmail.com>
2020-03-24 18:16:54 +0200

    Update one.h

--- dk92/Kshanovskaya_Maryna/Laba2/one.h
+++ dk92/Kshanovskaya_Maryna/Laba2/one.h
@@ -7,10 +7,9 @@ void bubbleSort(char a[], int n);
 void insertionSort(char arr[], int n);
 void selectionSort(char arr[], int n);
 
-void fprintResult(char label[],  int size);
-
+//void fprintResult(FILE *file, char label[],  int size);
 void swap(char *xp, char *yp) ;
 
-void Filewriter(int mass[], int quantity, char* name);
+void fprintResult(char array[],  int size);
 
 #endif
