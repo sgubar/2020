9715f5d55822fbe3135afecdd8489a80cc8c80dc
Dmytro <55019079+dmk0704@users.noreply.github.com>
2020-03-25 23:34:49 +0200

    Create dk_tool.h

---
+++ dk92/Komarenko Dmytro/Lab2/dk_tool.h
@@ -0,0 +1,7 @@
+#ifndef DK_TOOL_H_INCLUDED
+#define DK_TOOL_H_INCLUDED
+
+void BubbleSort (char *arr ,int length);
+void SelectionSort(char *arr ,int length);
+void InsertionSort (char *arr ,int length);
+#endif 
