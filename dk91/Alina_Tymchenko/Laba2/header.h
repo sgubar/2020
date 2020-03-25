a62a12e18b31ae369f49582951a2f8dc7b1dc40a
alina-alya <55531135+alina-alya@users.noreply.github.com>
2020-03-25 19:42:44 +0200

    There is my header file

--- dk91/Alina_Tymchenko/Laba2/header.h
+++ dk91/Alina_Tymchenko/Laba2/header.h
@@ -7,14 +7,11 @@
 #include <time.h>
 #include <conio.h>
 
-void bubble_sort(char *array, int n);
-void output_of_the_array(char* x, int size);
-void sort_insert(char *array, int size);
-void sort_selection(char *array, int size);
-void record_to_file(char *array, int size, char *name);
-int GetLetterAmountInFile (FILE* file);
-int GetCharsAmountInFile (FILE* file);
-char* loadFile (char *path , int *size);
+void bubble_sort(char *array, int size);
+void insertion_sort(char *array, int size);
+void selection_sort(char *array, int size);
+void step(char *i, char *j) ;
+void fprintResult(char *array,  int size, char fn[20]);
 
 
 #endif // HEADER_H_INCLUDED
