673d251e6bdab927a4dcf8de866e7dde3567d6da
kotelents <55530813+kotelents@users.noreply.github.com>
2020-03-25 18:16:39 +0200

    Update main.c

--- dk91/juliakotelenets/laba2/main.c
+++ dk91/juliakotelenets/laba2/main.c
@@ -16,22 +16,22 @@ int main(void)
         printf("File opened\n");
 
     fseek (txt_file, 0, SEEK_END);
-    int size = ftell(txt_file);
+    int length = ftell(txt_file);
 	fclose(txt_file);
-    printf("Length of file: %i elements\n" , size);
+    printf("Length of file: %i elements\n" , length);
 
     FILE *txt_file1 = fopen("Text.txt" , "rt");
 
 
-	char *arr = (char*)malloc(sizeof(char) * size );
-    fgets(arr, size, txt_file1);
+	char *arr = (char*)malloc(sizeof(char) * length );
+    fgets(arr, length, txt_file1);
     fclose(txt_file1);
     getchar();
 
     printf("Select type of sort:\n 1.Bubble Sort\n 2.Insertion Sort\n 3.Selection Sort\n");
     int type_of_sort = 0;
     scanf("%i" , &type_of_sort);
-     
+
     if ((type_of_sort == 1) || (type_of_sort == 2) || (type_of_sort == 3) )
     {
 clock_t start, stop;
@@ -40,15 +40,15 @@ start = clock ();
          switch(type_of_sort)
         {
             case 1:
-            BubbleSort ( arr , size);
+            BubbleSort ( arr , length);
             break;
 
             case 2:
-            InsertionSort ( arr , size);
+            InsertionSort ( arr , length);
             break;
 
             case 3:
-            SelectionSort(size , arr);
+            SelectionSort(length , arr);
             break;
         }
 
@@ -65,7 +65,7 @@ return 0;
 
     FILE *file = fopen("Tekst_sort.txt","wt");
 	int i = 0;
-	while(i<size)
+	while(i<length)
 	{
 		putc((int)arr[i],file);
 		i++;
