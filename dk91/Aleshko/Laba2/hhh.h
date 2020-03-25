429551aa981819ab84790a6172b6de32224de3d2
Amulvaro <55878542+Amulvaro@users.noreply.github.com>
2020-03-25 15:34:55 +0200

    Create hhh.h

---
+++ dk91/Aleshko/Laba2/hhh.h
@@ -0,0 +1,10 @@
+#ifndef HHH_H_INCLUDED
+#define HHH_H_INCLUDED
+
+void bubbleSort(int *m, int n);
+void selectionSort(int *m, int n);
+void insertionSort (int *m, int n);
+int out(int *m, int n);
+int Create(int *m, int n);
+
+#endif  HHH_H_INCLUDED
