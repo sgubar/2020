6bc7f247340401abd7f9393464ea3953d13ef6b3
Dmytro <55019079+dmk0704@users.noreply.github.com>
2020-03-25 23:34:29 +0200

    Create dk_tool.c

---
+++ dk92/Komarenko Dmytro/Lab2/dk_tool.c
@@ -0,0 +1,66 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <time.h>
+#include "dk_tool.h"
+
+
+void BubbleSort (char *arr , int length)
+{
+    int theOut;
+	for (theOut = length; theOut > 1; theOut --)
+	{
+		int theIn;
+		for (theIn = 0; theIn < theOut ; theIn ++)
+		{
+			if (arr[theIn] < arr[theIn + 1])
+			{
+				char theTmp = arr[theIn];
+				arr[theIn] = arr[theIn + 1];
+				arr[theIn + 1] = theTmp;
+			}
+		}
+	}
+
+}
+
+
+void SelectionSort(char *arr ,int length )
+{
+    int theOut;
+	for (theOut = 0; theOut < length - 1; theOut ++)
+	{
+		int theMaxIndex = theOut;
+		int theIn;
+		for (theIn = theOut + 1;theIn < length; theIn ++)
+		{
+			if (arr[theIn]>arr[theMaxIndex])
+			{
+				theMaxIndex = theIn;
+			}
+		}
+
+		char theTmp = arr[theOut];
+		arr[theOut] = arr[theMaxIndex];
+		arr[theMaxIndex] = theTmp;
+	}
+}
+
+
+
+void InsertionSort (char *arr ,int length)
+{
+    int theOut;
+
+	for (theOut = 1; theOut < length; theOut ++)
+	{
+		char theTmp = arr[theOut];
+		int theIn = theOut;
+		while (theIn > 0 && (arr[theIn-1] <= theTmp))
+		{
+			arr[theIn] = arr[theIn - 1];
+			-- theIn;
+		}
+			arr[theIn] = theTmp;
+	}
+}
