83155a7a03bc82a97895cf13fd6380bd44c4491f
ArtemHerashchenko <60616039+ArtemHerashchenko@users.noreply.github.com>
2020-03-25 12:44:19 +0200

    Create functions.h

---
+++ dk91/Artem_Herashchenko/Lab_2/functions.h
@@ -0,0 +1,8 @@
+#ifndef FUNCTIONS_H_INCLUDED
+#define FUNCTIONS_H_INCLUDED
+
+void bubbleSort(char *array, int size);
+void selectionSort(char *array, int size);
+void inclusionSort(char *array, int size);
+
+#endif // FUNCTIONS_H_INCLUDED
