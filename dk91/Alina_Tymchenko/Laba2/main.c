9d31e3762199230997eb6bedabdba23dfe1c7128
alina-alya <55531135+alina-alya@users.noreply.github.com>
2020-03-25 19:45:54 +0200

    There is my main file

--- dk91/Alina_Tymchenko/Laba2/main.c
+++ dk91/Alina_Tymchenko/Laba2/main.c
@@ -1,57 +1,48 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
+#include <math.h>
+#include <string.h>
 #include "header.h"
 
+int main() {
+     float s = 0 ;
+    FILE *myfile;
+    myfile = fopen ("lab2.txt", "r+");
+
+        fseek(myfile, 0, SEEK_END);
+        s = ftell(myfile);
+    printf("SIZE: %f\n", s);
+
+    fseek(myfile,0,SEEK_SET);
+    int size = s;
+       char a[size];
+
+    fgets(a, size, myfile);
+    printf(" %s\n", a);
 
 
-int main()
-{
-  float TimeStart,TimeStop;
-  int size = 0;
+fclose(myfile);
+clock_t time;
+char Array[50000];
 
-  printf("Lets start sorting!\n");
+    strcpy(Array, a);
+    time = clock();
+    bubble_sort(Array, size);
+    time = clock() - time;
+    printf("\n Time of Bubble Sort is  %f \n", (double)time/CLOCKS_PER_SEC);
 
-  char *bubble = loadFile("lab_2.txt" , &size);
+    time = clock();
+    insertion_sort(Array, size);
+    time = clock() - time;
+    printf(" Time of Insertion Sort is  %f \n", (double)time/CLOCKS_PER_SEC);
 
-  TimeStart = clock()/(float)CLOCKS_PER_SEC;
-  bubble_sort(bubble, size);
-  TimeStop = clock()/(float)CLOCKS_PER_SEC;
+    time = clock();
+    selection_sort(Array, size);
+    time = clock() - time;
+    printf(" Time of Selection Sort is  %f\n ", (double)time/CLOCKS_PER_SEC);
 
-  printf("\nBubble Sort: %f \n", TimeStop - TimeStart);
+return 0;
 
-
-
-  char *insert = loadFile("lab_2.txt" , &size);
-
-  TimeStart = clock()/(float)CLOCKS_PER_SEC;
-  insert_sort(insert, size);
-  TimeStop = clock()/(float)CLOCKS_PER_SEC;
-
-  printf("\nInsert Sort: %f \n", TimeStop - TimeStart);
-
-
-
-  char *selection = loadFile("lab_2.txt" , &size);
-
-  TimeStart = clock()/(float)CLOCKS_PER_SEC;
-  selection_sort(selection, size);
-  TimeStop = clock()/(float)CLOCKS_PER_SEC;
-
-  printf("\nSelection Sort: %f \n", TimeStop - TimeStart);
-
-
-
-  record_to_file(bubble, size, "bubble.txt");
-  record_to_file(insert, size, "insert.txt");
-  record_to_file(selection, size, "selection.txt");
-  printf("\nCheck created files:)\n");
-
-
-
-  free(bubble);
-  free(insert);
-  free(selection);
-
-  return 0;
 }
+
