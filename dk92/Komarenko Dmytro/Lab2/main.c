3ece450c52b1c3cb9ce8b70d2ffe681b8227a672
Dmytro <55019079+dmk0704@users.noreply.github.com>
2020-03-25 23:33:50 +0200

    Create main.c

---
+++ dk92/Komarenko Dmytro/Lab2/main.c
@@ -0,0 +1,82 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <time.h>
+#include "dk_tool.h"
+
+int main(void)
+{
+    FILE *txt_file = fopen("Text.txt", "rt");
+    if(txt_file == NULL)
+    {
+        printf("No such file found");
+        return -1;
+    }
+    else
+        printf("File opened\n");
+    fseek (txt_file, 0, SEEK_END);
+    int length = ftell(txt_file);
+	fclose(txt_file);
+    printf("Length of file: %i elements\n" , length);
+
+    FILE *txt_file1 = fopen("Text.txt" , "rt");
+
+
+	char *arr = (char*)malloc(sizeof(char) * length );
+    fgets(arr, length, txt_file1);
+    fclose(txt_file1);
+    getchar();
+
+    printf("Select type of sort:\n 1.Bubble Sort\n 2.Insertion Sort\n 3.Selection Sort\n");
+    int type_of_sort = 0;
+    scanf("%i" , &type_of_sort);
+
+    if ((type_of_sort == 1) || (type_of_sort == 2) || (type_of_sort == 3) )
+    {
+clock_t start, stop;
+start = clock ();
+
+         switch(type_of_sort)
+        {
+            case 1:
+            BubbleSort (arr, length);
+            break;
+
+            case 2:
+            InsertionSort (arr, length);
+            break;
+
+            case 3:
+            SelectionSort(arr, length);
+            break;
+        }
+
+stop = clock();
+printf("Time of sorting %.10lf seconds\n", (double)(stop - start) / CLOCKS_PER_SEC);
+
+    }
+    else
+        {
+            printf("Wrong! Select type of sort again!\n");
+            return type_of_sort;
+        }
+
+    FILE *file = fopen("Sorted_text.txt","wt");
+    if(txt_file == NULL)
+    {
+        printf("No such file found");
+        return -1;
+    }
+    else
+        printf("File opened\n");
+	int i = 0;
+	while(i<length)
+	{
+		putc((int)arr[i],file);
+		i++;
+	}
+	fclose(file);
+
+    free(arr);
+	return 0;
+}
