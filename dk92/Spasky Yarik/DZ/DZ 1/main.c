55510a6b86b261b699f6fcd614b303843681c28b
Jariksp <55439414+Jariksp@users.noreply.github.com>
2020-04-21 13:15:52 +0300

    Update main.c

--- dk92/Spasky Yarik/DZ/DZ 1/main.c
+++ dk92/Spasky Yarik/DZ/DZ 1/main.c
@@ -1,1 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <malloc.h>
+#include "dk_tools.h"
 
+int main(int argc, char *argv[])
+{
+int N;
+int matrix1[10][10];
+int matrix2[10][10];
+int result_matrix[10][10];
+
+printf("Enter array size: \n");
+scanf("%i", &N);
+
+FirstMatrix(N,matrix1);
+FirstMatrix(N,matrix2);
+resultOfmultiplication(N,matrix1,matrix2,result_matrix);
+print(N,result_matrix);
+
+
+return 0;
+}
