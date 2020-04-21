9ca6f1fd7f86d189ce2bbc399f9c84776ab603dd
Jariksp <55439414+Jariksp@users.noreply.github.com>
2020-04-21 13:17:59 +0300

    Update dk_tools.h

--- dk92/Spasky Yarik/DZ/DZ 1/dk_tools.h
+++ dk92/Spasky Yarik/DZ/DZ 1/dk_tools.h
@@ -1,1 +1,12 @@
+#ifndef dk_tools_h
+#define dk_tools_h
 
+#include <stdio.h>
+
+void FirstMatrix(int x, int matrix[10][10]);
+
+void resultOfmultiplication(int x, int matrix1[10][10], int matrix2[10][10], int result_matrix[10][10]);
+
+void result(int x, int result_matrix[10][10]);
+
+#endif
