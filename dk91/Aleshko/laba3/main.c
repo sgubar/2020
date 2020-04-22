b94afdf96750d9bf28da2bb3110dd2c7b2710eac
Amulvaro <55878542+Amulvaro@users.noreply.github.com>
2020-04-22 16:17:18 +0300

    Update main.c

--- dk91/Aleshko/laba3/main.c
+++ dk91/Aleshko/laba3/main.c
@@ -1,1 +1,18 @@
-
+#include <stdio.h>
+#include <stdlib.h>
+#include <iso646.h>
+#include <time.h>
+#include "hhh.h"
+int main()
+{
+    int n = 100;
+    int masive[n];
+    int i = 0;
+    Create(masive, n);
+    clock_t time;
+    time = clock();
+    quicksort(masive, 0, n-1);
+    time = clock() - time;
+    printf("%f ", (double)time/CLOCKS_PER_SEC);
+    out(masive, n);
+}
