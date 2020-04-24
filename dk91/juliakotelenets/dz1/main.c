aa2d23c863c5ef2e74d00bcc8c49de537c8762dd
kotelents <55530813+kotelents@users.noreply.github.com>
2020-04-24 22:12:02 +0300

    Update main.c

--- dk91/juliakotelenets/dz1/main.c
+++ dk91/juliakotelenets/dz1/main.c
@@ -1,3 +1,5 @@
+//Функція виконує перестановку 0 з n-1, 1 з n-2 …. елементів масиву
+
 #include <stdio.h>
 #include <stdlib.h>
 #include "dk_tool.h"
@@ -8,13 +10,13 @@ int main()
 	printf("Enter size of array:");
 	scanf("%i\n", &b);
 	a = (int*)malloc(b * sizeof(int));
-
+//вставлення елементів у оголошений масив
 	for (int i = 0; i < b; i++)
 		scanf("%d", &a[i]);
 
-	a = reversal(a, b);
+	a = reversal(a, b);    //присвоюємо масиву значення реверсу
 	printf("reverse\n");
-	for (int i = 0; i < b; i++)
+	for (int i = 0; i < b; i++) //друк зворотного масиву на консольний вихід
 		printf("%d\n", a[i]);
 
 	return 0;
