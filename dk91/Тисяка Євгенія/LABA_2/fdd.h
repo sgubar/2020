006b70fd1b4df1d0a645c304d207f13b678d9de3
Тисяк Євгеній <55532252+LAMPA23@users.noreply.github.com>
2020-04-27 21:32:40 +0300

    Create fdd.h

---
+++ dk91/Тисяка Євгенія/LABA_2/fdd.h
@@ -0,0 +1,10 @@
+#ifndef FDD_H_INCLUDED
+#define FDD_H_INCLUDED
+#include <stdio.h>
+#include <stdlib.h>
+int ggg(char* a);
+void fff(char *a, int n);
+void Insertion__Sort(char* a, int n);
+void Selection__Sort(char* a, int n);
+void Bubble__Sort(char* a, int n);
+#endif // FDD_H_INCLUDED
