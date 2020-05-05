b20b06987fc372211d79019849e6d99da5b1b773
Jariksp <55439414+Jariksp@users.noreply.github.com>
2020-05-05 14:03:05 +0300

    Update dk_tool.h

--- dk92/Spasky Yarik/LABA 3/dk_tool.h
+++ dk92/Spasky Yarik/LABA 3/dk_tool.h
@@ -1,1 +1,13 @@
+#ifndef sort_h
+#define sort_h
+
+void ShellSort(char *aArray, long  length);
+int binarysearch(char *aArray, long length, char val );
+void searchArray(char *aArray, long length);
+int mainProcess();
+void inPut( long length, char *aArray);
+ void Open();
+
+
+#endif
 
