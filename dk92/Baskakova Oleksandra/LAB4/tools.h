4ea136bab71a9d0d2efbfd654bce027cd85b944b
DeerAkira <54910051+DeerAkira@users.noreply.github.com>
2020-05-06 20:57:14 +0300

    Update tools.h

--- dk92/Baskakova Oleksandra/LAB4/tools.h
+++ dk92/Baskakova Oleksandra/LAB4/tools.h
@@ -1,8 +1,3 @@
-#include <stdlib.h>
-#include <stdio.h>
-#include <memory.h>
-
-unsigned int getUnsignedInt(char* label);
-void *createArray(int size);
-void fprintResult(FILE *file, char label[], int *result, int size);
-int linearSearch(int *array, int size, int element);
+#include "linked_list.h"
+List *load_from_file(char *path);
+void write_to_file(List *list, char *path);
