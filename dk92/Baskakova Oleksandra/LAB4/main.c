0c48536be6960895084117bc454c589302c8c54e
DeerAkira <54910051+DeerAkira@users.noreply.github.com>
2020-05-06 20:52:54 +0300

    Update main.c

--- dk92/Baskakova Oleksandra/LAB4/main.c
+++ dk92/Baskakova Oleksandra/LAB4/main.c
@@ -4,7 +4,9 @@
 int main(int argc, char const *argv[])
 {
     List *list = load_from_file("input.txt");
+    printf ("input.txt is open");
     list_reverse(&list);
     write_to_file(list, "output.txt");
+    printf ("reversed text was written to open.txt");
     return 0;
 }
