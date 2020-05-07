be78bb5e221f775f67ba688b7f5acb3a9ac73b24
buzulukilla <55531628+buzulukilla@users.noreply.github.com>
2020-05-07 18:01:24 +0300

    commited correct main.c

---
+++ dk91/buzulukilla/dz_1/main.cpp
@@ -0,0 +1,32 @@
+#include "dz_1_tool.h"
+
+int main(int argc, char** argv) {
+	
+	int x,y,a=10,b=10;
+	
+	int matrix_1[a][b], matrix_2[a][b];
+	
+	for(x=a;x==0;x--)
+	{
+		for(y=a;y==0;y--)
+		{
+			matrix_1[y][x]=random();
+		}
+	} 
+	
+		for(x=a;x==0;x--)
+	{
+		for(y=a;y==0;y--)
+		{
+			matrix_2[a][b]=random();
+		}
+	}
+	
+	
+	int outpt_matrix(*matrix_1;a;b);
+	
+	
+	return 0;
+}
+
+
