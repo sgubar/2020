37da89f469cfebb60102306951c8f0f68f788214
Jariksp <55439414+Jariksp@users.noreply.github.com>
2020-05-07 16:12:06 +0300

    Update main.c

--- dk92/Spasky Yarik/LAB 5/main.c
+++ dk92/Spasky Yarik/LAB 5/main.c
@@ -1,1 +1,31 @@
+#include <stdio.h>
+#include "dk_tool.h"
+
+int main() {
+    intTree *LAB5 = createIntTree();
+
+    insertIntValueToTree(LAB5, 15);
+    insertIntValueToTree(LAB5, 2);
+    insertIntValueToTree(LAB5, 89);
+    insertIntValueToTree(LAB5, 56);
+    insertIntValueToTree(LAB5, 13);
+    insertIntValueToTree(LAB5, 8);
+    insertIntValueToTree(LAB5, 43);
+    insertIntValueToTree(LAB5, 3);
+
+    printTree(LAB5->root);
+    printf("\n");
+
+    printf("Find node : %d\n", findNodeWithValue(LAB5, 89)-> value);
+
+    deleteValueFromTree(LAB5, 3);
+    deleteValueFromTree(LAB5, 13);
+    deleteValueFromTree(LAB5, 1);
+
+    printTree(LAB5->root);
+    printf("\n");
+
+    deleteIntTree(LAB5);
+
+}
 
