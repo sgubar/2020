253c9e691607b1ef6e2df2e6dcf1fa3666549818
Jariksp <55439414+Jariksp@users.noreply.github.com>
2020-05-07 16:13:00 +0300

    Update dk_tool.h

--- dk92/Spasky Yarik/LAB 5/dk_tool.h
+++ dk92/Spasky Yarik/LAB 5/dk_tool.h
@@ -1,1 +1,29 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef struct __node intNode;
+
+struct __node
+{
+    int value;
+    intNode *leftChild;
+    intNode *rightChild;
+};
+
+typedef struct __tree
+{
+    intNode *root;
+    int count;
+}intTree;
+
+intTree *createIntTree();
+void deleteIntTree(intTree *aTree);
+void printTree(intNode *aRoot);
+
+void insertIntValueToTree(intTree *aTree, int aValue);
+int deleteValueFromTree(intTree *aTree, int aValue);
+
+intNode *findNodeWithValue(intTree *aTree ,int aValue);
+
+
 
