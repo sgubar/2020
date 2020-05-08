6c665de04c036d4ada6f06c2b2c683395ac97501
Jariksp <55439414+Jariksp@users.noreply.github.com>
2020-05-08 18:41:04 +0300

    Update main.c

--- dk92/Spasky Yarik/DZ/DZ2/main.c
+++ dk92/Spasky Yarik/DZ/DZ2/main.c
@@ -1,1 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "dk_tool.h"
 
+
+int main()
+{
+	  Point A = {-5, 0};
+    Point B = {0, 2};
+    Point C = {5, 0};
+    Point D = {0, -2};
+    Point Centre = {0,0};
+
+
+	Ellipse *Ellips = createEllipse(&A, &B, &C, &D, &Centre);
+
+    if(Ellips == NULL)
+        printf("ERROR\n");
+    else
+            printf("The Square of Ellipse = %d\n", Ellipse_Area(Ellips));
+
+
+	destroyEllipse(Ellips);
+}
