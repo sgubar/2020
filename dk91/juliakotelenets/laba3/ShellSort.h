b3e280d1303c4c88a887b479d87705385cd0e4c5
kotelents <55530813+kotelents@users.noreply.github.com>
2020-05-11 20:35:48 +0300

    Create ShellSort.h

---
+++ dk91/juliakotelenets/laba3/ShellSort.h
@@ -0,0 +1,10 @@
+#ifndef ShellSort_h
+#define ShellSort_h
+
+#include <stdio.h>
+
+void shellSort(char *mass, int Count);
+void WriteToFile(char *mass, int asize, char *fname);
+void randomizer (char *mass, int size);
+int BinarySearch(int value, char *mass, int asize );
+#endif /* ShellSort_h */
