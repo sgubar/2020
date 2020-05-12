cbc13f8b41ef53cc779d9174ed71adff0b399d6a
FilipRatushniak <55531346+FilipRatushniak@users.noreply.github.com>
2020-05-12 20:42:32 +0300

    Create header.h

---
+++ dk91/Filip/Laba3/header.h
@@ -0,0 +1,9 @@
+#ifndef HHH_H_INCLUDED
+#define HHH_H_INCLUDED
+
+void quicksort(int *m,  int first, int last);
+int out(int *m, int n);
+int Create(int *m, int n);
+int poisk(int *m, int n, int key);
+
+#endif  HHH_H_INCLUDED
