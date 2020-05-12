e876d3a3815ece66336ff48bfa6f350e25ded781
Jariksp <55439414+Jariksp@users.noreply.github.com>
2020-05-12 17:10:55 +0300

    Update dk_ellipse3.h

--- dk92/Spasky Yarik/DZ/DZ3/dk_ellipse3.h
+++ dk92/Spasky Yarik/DZ/DZ3/dk_ellipse3.h
@@ -1,1 +1,33 @@
 
+#ifndef DK_ELLIPSE3_H
+#define DK_ELLIPSE3_H
+
+
+#include <stdio.h>
+#include <malloc.h>
+#include <math.h>
+
+typedef struct tagPoint
+{
+    int x;
+    int y;
+}Point;
+
+typedef struct tagEllipse
+{
+    Point *A;
+    Point *B;
+    Point *C;
+    Point *D;
+    Point *Centre;
+}Ellipse;
+
+Ellipse *createEllipse(Point *A, Point *B, Point *C, Point *D, Point *Centre);
+void destroyEllipse(Ellipse *eEllipse);
+void writeEllipse(FILE *eFile, Ellipse *eEllipse);
+float Ellipse_Area(Ellipse *eEllipse);
+Point *copyPointWithPoint(Point *ePoint);
+
+
+#endif
+
