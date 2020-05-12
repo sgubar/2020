10d0ebaaa8aaedd8b2c99fc77c6c4fd79e3f3e9b
Jariksp <55439414+Jariksp@users.noreply.github.com>
2020-05-12 19:35:37 +0300

    Update main.c

--- dk92/Spasky Yarik/LABA 4/main.c
+++ dk92/Spasky Yarik/LABA 4/main.c
@@ -1,1 +1,31 @@
+#include <stdio.h>
+#include "LinkedNode.h"
+#include "SingleLinked_List.h"
+#include "sort.h"
+
+
+    int main()
+{
+	IntList *thelist = CreateList();
+	int value;
+	int i;
+	printf("\n\nAdd elements :\n\n");
+			for(i = 0; i < 9; i++)
+		 	{
+               printf("Node [ %d ] - ", i);
+			   scanf("%d", &value);
+               IntNode *theNode = CreateNode(value);
+               AddNode(thelist, theNode);
+			}
+		doPrintList(thelist);
+
+		printf("\n MIN = Node [ %d ]\n\n", Minimal(thelist));
+
+        Sort(thelist, Minimal(thelist),thelist->acount-1 );
+
+
+		doPrintList(thelist);
+
+		return 0;
+}
 
