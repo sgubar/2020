53b6a6fa0c7b194f2e8c24f78363fddeda47e579
ArtemHerashchenko <60616039+ArtemHerashchenko@users.noreply.github.com>
2020-05-13 14:45:35 +0300

    Update functions.c

--- dk91/Artem_Herashchenko/Lab_3/functions.c
+++ dk91/Artem_Herashchenko/Lab_3/functions.c
@@ -1,1 +1,38 @@
-/
+#include "functions.h"
+
+void ShallSort(char *array, int size)
+{
+    int i, j, step;
+    int tmp;
+    for (step = size / 2; step > 0; step /= 2)
+        for (i = step; i < size; i++)
+        {
+            tmp = array[i];
+            for (j = i; j >= step; j -= step)
+            {
+                if (tmp < array[j - step])    
+                    array[j] = array[j - step];
+                else
+                    break;
+            }
+            array[j] = tmp;
+        }
+}
+
+int linearSearch(char *array, char simvol)
+{
+    if(array != 0)
+    {
+        for (int i = 0; i < strlen(array); ++i)
+        {
+         if(array[i] == simvol)
+             return i;
+        }
+        return -1;
+    }
+}
+
+
+
+
+
