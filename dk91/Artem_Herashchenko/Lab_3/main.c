dd6c2e91d280e0ea42a4cd13640175010cf3ff7c
ArtemHerashchenko <60616039+ArtemHerashchenko@users.noreply.github.com>
2020-05-13 14:44:33 +0300

    Update main.c

--- dk91/Artem_Herashchenko/Lab_3/main.c
+++ dk91/Artem_Herashchenko/Lab_3/main.c
@@ -1,1 +1,34 @@
-/
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include "functions.h"
+
+int main()
+{
+int size = 20000;
+char simvol;
+char array_1[size], array_2[size];
+
+FILE *sorting;
+sorting = fopen("sorting.txt", "r+");
+
+fscanf(sorting, "%s", array_1);
+
+strcpy(array_2, array_1);
+
+printf("\nEnter odin simvol for searching: \n");
+scanf("%c", &simvol);
+printf("Index : [%i]\n", linearSearch(array_2, simvol));
+
+clock_t start = clock();
+ShallSort(array_2, size);
+clock_t end = clock();
+printf ("Shall = %.10lf\n", (double)(end - start)/CLOCKS_PER_SEC);
+fprintf(sorting, "\n\nShall:\n");
+fputs(array_2, sorting);
+
+
+fclose(sorting);
+
+return 0;
+}
