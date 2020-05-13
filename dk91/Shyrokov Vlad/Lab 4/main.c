d569133d8c0ff67e0fe31b1565d338a59931175c
Vladosss-byte <55531375+Vladosss-byte@users.noreply.github.com>
2020-05-13 04:41:21 +0300

    Update main.c

--- dk91/Shyrokov Vlad/Lab 4/main.c
+++ dk91/Shyrokov Vlad/Lab 4/main.c
@@ -1,1 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <iso646.h>
+#include <time.h>
+#include "hhh.h"
 
+int main(int argc, char const *argv[])
+{
+    List *list = load_from_file("input.txt");
+    listprint(list);
+    printf("\n");
+    listprintr(list);
+    return 0;
+}
