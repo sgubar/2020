be6f9a6569b9ffc746ef248c82252dec54ec1713
Тисяк Євгеній <55532252+LAMPA23@users.noreply.github.com>
2020-05-14 15:40:33 +0300

    Create SSS.h

---
+++ dk91/Тисяка Євгенія/LABA_3/SSS.h
@@ -0,0 +1,9 @@
+#ifndef SSS_H_INCLUDED
+#define SSS_H_INCLUDED
+void ShellSort(int n, int mass[]);
+int poisk(int *m, int n, int key);
+int revice(char *mass, int n);
+int ggg(char* a);
+void fff(char *a, int n);
+
+#endif // SSS_H_INCLUDED
