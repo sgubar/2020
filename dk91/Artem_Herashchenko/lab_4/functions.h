3b08b66aaa8a1fc49a60b63790ab26f42b62f271
ArtemHerashchenko <60616039+ArtemHerashchenko@users.noreply.github.com>
2020-05-15 17:35:06 +0300

    Update functions.h

--- dk91/Artem_Herashchenko/lab_4/functions.h
+++ dk91/Artem_Herashchenko/lab_4/functions.h
@@ -1,1 +1,16 @@
+#ifndef HHH_H_INCLUDED
+#define HHH_H_INCLUDED
+
+typedef struct list              //саздаём структуру
+{
+    int field;
+    struct list *next;
+} List;
+
+List *init(int val);            //создаём первый элемент
+List *load_from_file(char *path);   //берём данные с файла
+List* addelem(List *list, int val);  //добавляем элемент
+void printList(List* list, int a, int b);    //вывод списка
+
+#endif // HHH_H_INCLUDED
 
