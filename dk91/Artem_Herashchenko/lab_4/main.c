34e92cfe49ea75fccb1ac45c7ca2094b3a6cafe5
ArtemHerashchenko <60616039+ArtemHerashchenko@users.noreply.github.com>
2020-05-15 17:35:33 +0300

    Update main.c

--- dk91/Artem_Herashchenko/lab_4/main.c
+++ dk91/Artem_Herashchenko/lab_4/main.c
@@ -1,1 +1,15 @@
-/
+#include <stdio.h>
+#include <stdlib.h>
+#include "hhh.h"
+
+int main(int argc, char const *argv[])
+{
+	int a, b;
+	printf("Vvedite granici\n");
+    List *list = load_from_file("input.txt"); //создание структуры
+    scanf("%d", &a);
+    scanf("%d", &b);
+    printList(list, a, b);
+    return 0;
+}
+
