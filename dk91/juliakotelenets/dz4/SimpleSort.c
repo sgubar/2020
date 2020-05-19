ce95823fd674aae3662be894acb73f70d7567355
kotelents <55530813+kotelents@users.noreply.github.com>
2020-05-19 21:39:52 +0300

    Update SimpleSort.c

--- dk91/juliakotelenets/dz4/SimpleSort.c
+++ dk91/juliakotelenets/dz4/SimpleSort.c
@@ -1,19 +1,18 @@
-#include <stdio.h>
-#include <stdlib.h>
+#include "dk_tool_array.h"
 #include "SimpleSort.h"
 
 
-void bubbleSort(int anArray[], int aSize)
+void bubbleSort(EllipseArray *anArray)
 {
-	for (int theOut = aSize - 1; theOut >= 1; theOut --)
+	for (int theOut = anArray->count - 1; theOut >= 1; theOut --)
 	{
 		for (int theIn = 0; theIn < theOut; theIn ++)
 		{
-			if (anArray[theIn] > anArray[theIn + 1])
+			if (SquareEllipse(anArray->ellipses[theIn]) > SquareEllipse(anArray->ellipses[theIn + 1]))
 			{
-				int theTmp = anArray[theIn];
-				anArray[theIn] = anArray[theIn + 1];
-				anArray[theIn + 1] = theTmp;
+			    Ellipse *theTmp = anArray->ellipses[theIn];
+				anArray->ellipses[theIn] = anArray->ellipses[theIn + 1];
+				anArray->ellipses[theIn + 1] = theTmp;
 			}
 		}
 	}
