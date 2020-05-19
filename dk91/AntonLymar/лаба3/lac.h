0b8d06b0cfecbcae8bc38aa27b369a71e816a48b
Antonlao <55503708+Antonlao@users.noreply.github.com>
2020-05-19 20:17:45 +0300

    Create lac.h

---
+++ dk91/AntonLymar/лаба3/lac.h
@@ -0,0 +1,9 @@
+#ifndef LAC_H_INCLUDED
+#define LAC_H_INCLUDED
+
+void Shells(int n, char mass[]);
+int poisk(char *m, int n, char key);
+int proverka(char *mass, int n);
+void vuvod(char *a, int n);
+
+#endif // LAC_H_INCLUDED
