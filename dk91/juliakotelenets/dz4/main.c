412bbb966c173edef78bd35dc21e5f7b3557beb7
kotelents <55530813+kotelents@users.noreply.github.com>
2020-05-19 21:41:02 +0300

    Update main.c

--- dk91/juliakotelenets/dz4/main.c
+++ dk91/juliakotelenets/dz4/main.c
@@ -15,6 +15,7 @@ int main()
 
 	Ellipse *El1 = createEllipse(&Centre, &R, &r);
     addElement(theArray, El1);
+     //   printf("The Square of Ellipse = %f\n", SquareEllipse(El1));
 
 
 	R = 50;
@@ -22,53 +23,61 @@ int main()
 
     El1 = createEllipse(&Centre, &R, &r);
     addElement(theArray, El1);
+      //  printf("The Square of Ellipse = %f\n", SquareEllipse(El1));
+
 
 	R = 30;
 	r = 20;
 
     El1 = createEllipse(&Centre, &R, &r);
     addElement(theArray, El1);
+      //  printf("The Square of Ellipse = %f\n", SquareEllipse(El1));
+
 
 	R = 60;
 	r = 50;
 
     El1 = createEllipse(&Centre, &R, &r);
     addElement(theArray, El1);
+       // printf("The Square of Ellipse = %f\n", SquareEllipse(El1));
+
 
     R = 70;
 	r = 30;
 
     El1 = createEllipse(&Centre, &R, &r);
     addElement(theArray, El1);
+    //printf("The Square of Ellipse = %f\n", SquareEllipse(El1));
     printArray(theArray);
 
 	FILE* theFile = fopen("test.json", "a+");
 	if (theFile != NULL)
     {
-		printf("\n File successfully created!\n\n");
+		printf("\n File successfully created!");
     }
 	else
 		return 0;
 
 	writeArrayToJSON(theFile, theArray);
 
+
 	int i;
 	for(i=0; i<theArray->count; i++)
     {
         SquareEllipseArray[i]=round(SquareEllipse(theArray->ellipses[i]));
-        printf("\n EllipseSquare[%d]=%d\n", i, SquareEllipseArray[i]);
+        printf("\n EllipseSquare[%d]=%d", i, SquareEllipseArray[i]);
     }
-    bubbleSort(SquareEllipseArray, theArray->count);
-    printf("\n\n Sorted! \n");
-
-    for(i = 0; i < theArray->count; i++)
+    bubbleSort(theArray);
+    printf(" \n Sorted! \n");
+int n;
+   for(n = 0; n < theArray->count; n++)
     {
-        printf("\n EllipseSquare[%d]=%d\n", i, SquareEllipseArray[i]);
+        SquareEllipseArray[n]=round(SquareEllipse(theArray->ellipses[n]));
+        printf("\n EllipseSquare[%d]=%d", n, SquareEllipseArray[n]);
     }
-
+    freeArray(theArray);
     fflush(theFile);
     fclose(theFile);
-    freeArray(theArray);
 
 	return 0;
 }
