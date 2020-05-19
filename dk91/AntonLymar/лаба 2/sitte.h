b3ea95986180c6389d04ae823d777d1a4fd12234
Antonlao <55503708+Antonlao@users.noreply.github.com>
2020-05-19 12:17:46 +0300

    Create sitte.h

---
+++ dk91/AntonLymar/лаба 2/sitte.h
@@ -0,0 +1,10 @@
+#ifndef SITTE_H_INCLUDED
+#define SITTE_H_INCLUDED
+#include <stdio.h>
+#include <stdlib.h>
+int ggg(char* a);
+void fff(char *a, int n);
+void Insertion__Sort(char* a, int n);
+void Selection__Sort(char* a, int n);
+void Bubble__Sort(char* a, int n);
+#endif // SITTE_H_INCLUDED
