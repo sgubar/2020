ccb7af79aab0f347ff11486175e2b197c1f6b9e1
kotelents <55530813+kotelents@users.noreply.github.com>
2020-05-20 14:34:17 +0300

    Create dk_tool.h

---
+++ dk91/juliakotelenets/dz5/dk_tool.h
@@ -0,0 +1,30 @@
+#ifndef dk_tool_h
+#define dk_tool_h
+#include <stdio.h>
+
+// Point description
+typedef struct tagPoint
+{
+	int x;
+	int y;
+}Point;
+
+// Line description
+typedef struct tagEllipse
+{
+	Point *O;
+	int R;
+    int r;
+}Ellipse;
+
+// Init interface
+Ellipse *createEllipse(Point *Centre, int *R, int *r);
+void destroyEllipse(Ellipse *aEllipse);
+float SquareEllipse(Ellipse *aEllipse);
+void printEllipse(Ellipse *aEllipse);
+Point *copyPointWithPoint(Point *aPoint);
+void writeEllipseToJSON(FILE *aFile, Ellipse *aEllipse);
+void writePointToJSON(FILE *aFile, Point *aPoint);
+
+#endif
+
