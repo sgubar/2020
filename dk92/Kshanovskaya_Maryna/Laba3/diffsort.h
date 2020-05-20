5755567ac950f57e7e20e2f9797f41adeed03d7e
kshanovska <marinakshanovska@gmail.com>
2020-05-21 02:59:42 +0300

    Create diffsort.h

---
+++ dk92/Kshanovskaya_Maryna/Laba3/diffsort.h
@@ -0,0 +1,9 @@
+#ifndef diffsort_h
+#define diffsort_h
+
+#include <stdio.h>
+
+void shellSort(char *mass, int Count);
+void randomizer (char *mass, int size);
+int lineSearch(char *array, int size, char key);
+#endif 
