884605627367b13fa176721687da14436146cb03
kotelents <55530813+kotelents@users.noreply.github.com>
2020-05-20 14:34:39 +0300

    Create dk_tool_array.c

---
+++ dk91/juliakotelenets/dz5/dk_tool_array.c
@@ -0,0 +1,89 @@
+#include "dk_tool_array.h"
+#include "dk_tool.h"
+#include <stdio.h>
+#include <stdlib.h>
+//init/dealloc
+EllipseArray *createArray(int aNumber)
+{
+    EllipseArray *theResult = NULL;
+    if (aNumber>0)
+    {  // выделение памяти под корень списка
+        theResult=(EllipseArray *)malloc(sizeof(EllipseArray));
+        if(NULL != theResult)
+        {
+            theResult->ellipses = (Ellipse **)malloc(sizeof(Ellipse *)*aNumber);
+            if(NULL != theResult->ellipses)
+            {
+                // Присваивание значения узлу
+                theResult->number = aNumber;
+                theResult->count = 0;
+            }
+            else
+            {   //осводождаем память
+                free(theResult);
+                theResult=NULL;
+            }
+        }
+    }
+    return theResult;
+}
+
+void freeArray(EllipseArray *anArray)  //освобождаем массив
+{
+    if(NULL != anArray)
+    {
+        for( int i=0; i<anArray->count; i++)
+        {
+            destroyEllipse(anArray->ellipses[i]);  //разрушаем элипсы
+        }
+            free(anArray->ellipses); //освобождаем память от массива элипсов
+            free(anArray);  //освобождаем память от массива
+    }
+}
+//interface
+//возвращает индекс добавленного элемента или -1 в случае неудачи
+int addElement(EllipseArray *anArray, Ellipse *aEllipse)
+{
+    int theResult = -1;   //придаем значение -1
+    if(NULL != anArray && NULL != aEllipse && anArray->count < anArray->number)
+    {
+        anArray->ellipses[anArray->count]=aEllipse;
+        theResult = anArray->count;
+        anArray->count++;
+    }
+    return theResult;
+}
+void writeArrayToJSON(FILE *aFile, EllipseArray *anArray)
+{
+    if(NULL == anArray || NULL == aFile)
+    {
+        return;
+    }
+    fprintf(aFile, "{\n\"number\" : %d, \n\"count\" : %d, \n", anArray->number, anArray->count);
+    fprintf(aFile, "\"ellipses\" : %d\n[\n");
+    for(int i=0; i<anArray->count; i++)
+    {
+        writeEllipseToJSON(aFile, anArray->ellipses[i]);
+        if(i<anArray->count-1)
+        {
+            fprintf(aFile, ",");
+        }
+        fprintf(aFile, "\n");
+    }
+    fprintf(aFile, "]\n}");
+}
+void printArray(EllipseArray* anArray)
+{
+	if (anArray != NULL)
+	{
+		printf("\n [EA]: number(%d), count(%d)", anArray->number, anArray->count);
+		printf(", ellipses: (%d) \n");
+
+		for (int i = 0; i < anArray->count; i++)
+		{
+			printEllipse(anArray->ellipses[i]);
+		}
+	}
+	else return;
+}
+
