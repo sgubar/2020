bfe61d99bb88177093b892ff6e014d09cbf269c3
kshanovska <marinakshanovska@gmail.com>
2020-05-21 03:00:31 +0300

    Update diffsort.h

--- dk92/Kshanovskaya_Maryna/Laba3/diffsort.h
+++ dk92/Kshanovskaya_Maryna/Laba3/diffsort.h
@@ -3,6 +3,10 @@
 #include <stdio.h>
 
 void shellSort(char *mass, int Count);
-void randomizer (char *mass, int size);
+
 int lineSearch(char *array, int size, char key);
+
+void randomizer (char *mass, int size);
+
+
 #endif 
