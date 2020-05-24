e06def302e0bae194794c1002da374f6099f12fd
Gall8965 <55531309+Gall8965@users.noreply.github.com>
2020-05-24 23:49:22 +0300

    Update func.c

--- dk91/KirMisha/Lab4/func.c
+++ dk91/KirMisha/Lab4/func.c
@@ -1,18 +1,18 @@
 #include "func.h"
 
 void FillWithData(Node** list, const Data data) {
-    Node* node = malloc(sizeof(Node)); //Выделение памяти
-    node->data = data; //Заносим данные в ячейку
-    node->next = *list; //Этой ячейке даём предыдущий адрес
-    *list = node; //А новая ячейка потом получит следующий адрес
+    Node* node = malloc(sizeof(Node)); 
+    node->data = data; 
+    node->next = *list; 
+    *list = node; 
 }
-void Straight(const Node* list) { //Функция вывода в прямом порядке
+void Straight(const Node* list) { 
     if (list) {
         printf("%d ", list->data);
         Straight(list->next);
     }
 }
-void Reverse(const Node* list) { //Функция вывода в обратном порядке
+void Reverse(const Node* list) { 
     if (list) {
         Reverse(list->next);
         printf("%d ", list->data);
