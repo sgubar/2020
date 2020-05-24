81f355eaece700160d2861552dfc37a04c256b98
Gall8965 <55531309+Gall8965@users.noreply.github.com>
2020-05-24 23:48:57 +0300

    Update func.h

--- dk91/KirMisha/Lab4/func.h
+++ dk91/KirMisha/Lab4/func.h
@@ -6,14 +6,14 @@
 
 typedef int Data;
  
-//Создание типа, для того, чтобы не вызывать постоянно одно и то же
+
 typedef struct Node {
     Data data;
     struct Node* next;
 }   Node;
 
 
-void FillWithData(Node** list, const Data data); //Функция ввода данных в список
-void Straight(const Node* list); //Вывод с головы
-void Reverse(const Node* list); //Вывод с хвоста
+void FillWithData(Node** list, const Data data); /
+void Straight(const Node* list); 
+void Reverse(const Node* list); 
 #endif /* func_h */
