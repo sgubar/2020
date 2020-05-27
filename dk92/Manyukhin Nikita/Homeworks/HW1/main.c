40aaf4641d7495b55eb351a295c4a0e0963e6a36
cellentan <55437609+cellentan@users.noreply.github.com>
2020-05-28 02:22:56 +0300

    Add files via upload

---
+++ dk92/Manyukhin Nikita/Homeworks/HW1/main.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include "tools.h"
+
+int main() {
+
+    //Перетворити символьний вираз 8-го числа (не більше 4 символів) в число
+    int OctalNumber = 0;
+
+       printf("The minimum number is -9999, the maximum is 9999\nEnter the number to convert: ");
+
+    do
+    {
+        scanf("%d", &OctalNumber);
+        if (OctalNumber < -9999 || OctalNumber > 9999) printf("\n####ERROR####\n\nThe number does not meet the condition.\nEnter another: ");
+    }while(OctalNumber < -9999 || OctalNumber > 9999);
+
+
+    OctalConvert(OctalNumber);
+
+    return 0;
+}
