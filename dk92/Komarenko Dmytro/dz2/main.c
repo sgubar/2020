c37d0452b337d423d3507f5dab05e2d641f12a8e
Dmytro <55019079+dmk0704@users.noreply.github.com>
2020-05-29 19:44:59 +0300

    Update main.c

--- dk92/Komarenko Dmytro/dz2/main.c
+++ dk92/Komarenko Dmytro/dz2/main.c
@@ -1,1 +1,20 @@
+#include <stdio.h>
+#include "dk_tool.h"
 
+int main()
+{
+    system("chcp 1251");
+    system("cls");
+	Point Centre = {50,50};
+	int R = 40;
+    int r = 20;
+
+	Ellipse *El1 = createEllipse(&Centre, &R, &r);
+
+	printEllipse(El1);
+
+	printf("Площа еліпса = %f\n", SquareEllipse(El1));
+
+	destroyEllipse(El1);
+	return 0;
+}
