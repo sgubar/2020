2eb2ce74e578b43cf4e724526f2f521927a76082
Dmytro <55019079+dmk0704@users.noreply.github.com>
2020-05-29 18:24:48 +0300

    Create dk_tool.h

---
+++ dk92/Komarenko Dmytro/Lab3/dk_tool.h
@@ -0,0 +1,9 @@
+#ifndef DK_TOOL_H_INCLUDED
+#define DK_TOOL_H_INCLUDED
+
+void shellSort(char *mass, int Count);
+void randomizer (char *mass, int size);
+int BinarySearch(int value, char *mass, int asize );
+
+
+#endif // DK_TOOL_H_INCLUDED
