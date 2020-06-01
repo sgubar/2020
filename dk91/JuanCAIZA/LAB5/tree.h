8adaee55cf5328e5d76d241c5f12153635e1b450
Juan-Caiza06 <55527656+Juan-Caiza06@users.noreply.github.com>
2020-06-01 20:02:22 +0300

    Create tree.h

---
+++ dk91/JuanCAIZA/LAB5/tree.h
@@ -0,0 +1,19 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+typedef struct node
+{
+    struct node *left_node, *right_node;
+    int value;
+     //added only for displaying purpose
+    int nesting;
+    char* positon;
+
+} Node;
+
+Node *create_node(int value);
+void symetric_print(Node *n);
+void insert_node(Node **root, Node *child);
+Node *search_value(Node *root, int value);
+int delete_node(Node **root, Node *node_to_delete);
+void delete_tree(Node *root);
