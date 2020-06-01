ab466b695f5d5977f8e923ad4f4708ac4b376956
Juan-Caiza06 <55527656+Juan-Caiza06@users.noreply.github.com>
2020-06-01 18:53:03 +0300

    Update buble.h

--- dk91/JuanCAIZA/LAB2/buble.h
+++ dk91/JuanCAIZA/LAB2/buble.h
@@ -1,32 +1,7 @@
-#include "buble.h"
+#include "sortResult.c"
+#include "tools.h"
+#include "time.h"
 
-SortResult bubleSort(int *array, int size)
-{
-    int *arr = copyArray(array, size);
 
-    time_t t0 = time(0);
-    buble(arr, size);
-    time_t t1 = time(0);
-
-    SortResult result;
-    result.ellapsedTime = difftime(t1, t0);
-    result.array = arr;
-    return result;
-}
-
-void buble(int *array, int size)
-{
-    int swaps;
-    do
-    {
-        swaps = 0;
-        for (int i = 0; i < size - 1; i++)
-        {
-            if (array[i] > array[i + 1])
-            {
-                swap(array, i, i + 1);
-                swaps++;
-            }
-        }
-    } while (swaps != 0);
-}
+SortResult bubleSort(int *array, int size);
+void buble(int *array, int size);
