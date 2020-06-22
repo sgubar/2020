ca471899847d9f52f67071ee3a65db58f0c614a4
YuriiSak <55512856+YuriiSak@users.noreply.github.com>
2020-06-22 09:14:33 +0300

    Create function.c

---
+++ dk91/SakYR/LAB_3/function.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include <string.h>
+#include "functions.h"
+
+void ShallSort(char *array, int size)
+{
+    int i, j, step;
+    int tmp;
+    for (step = size / 2; step > 0; step /= 2)
+        for (i = step; i < size; i++)
+        {
+            tmp = array[i];
+            for (j = i; j >= step; j -= step)
+            {
+                if (tmp < array[j - step])
+                    array[j] = array[j - step];
+                else
+                    break;
+            }
+            array[j] = tmp;
+        }
+}
+
+int linearSearch(char *array, char simvol)
+{
+    if(array != 0)
+    {
+        for (int i = 0; i < strlen(array); ++i)
+        {
+         if(array[i] == simvol)
+             return i;
+        }
+        return -1;
+    }
+}
