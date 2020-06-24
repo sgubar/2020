b77d8c8cb7d070673941e78e207ccceb9dd5c445
Antonlao <55503708+Antonlao@users.noreply.github.com>
2020-06-24 21:00:16 +0300

    Create rer.c

---
+++ dk91/AntonLymar/lab5/rer.c
@@ -0,0 +1,138 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+#include "rer.h"
+
+Node* getFreeNode(T value, Node *parent) //функція для створення ноди
+{
+    Node* tmp = (Node*) malloc(sizeof(Node));
+    tmp->left = tmp->right = NULL;
+    tmp->data = value;
+    tmp->parent = parent;
+    return tmp;
+}
+
+Node* getNodeByValue(Node *root, T value)//Функція для пошуку ноди з таким же значенням, для видалення
+{
+    while (root) {
+        if (root->data > value) {
+            root = root->left;
+            continue;
+        } else if (root->data < value) {
+            root = root->right;
+            continue;
+        } else {
+            return root;
+        }
+    }
+    return NULL;
+}
+Node* getMinNode(Node *root)
+{
+    while (root->left) {
+        root = root->left;
+    }
+    return root;
+}
+
+Node* getMaxNode(Node *root)
+{
+    while (root->right) {
+        root = root->right;
+    }
+    return root;
+}
+void removeNode(Node *target)
+{
+    if (target->left && target->right) {
+        Node *localMax = getMaxNode(target->left);
+        target->data = localMax->data;
+        removeNode(localMax);
+        return;
+    } else if (target->left) {
+        if (target == target->parent->left) {
+            target->parent->left = target->left;
+        } else {
+            target->parent->right = target->left;
+        }
+    } else if (target->right) {
+        if (target == target->parent->right) {
+            target->parent->right = target->right;
+        } else {
+            target->parent->left = target->right;
+        }
+    } else {
+        if (target == target->parent->left) {
+            target->parent->left = NULL;
+        } else {
+            target->parent->right = NULL;
+        }
+    }
+    free(target);
+}
+
+void deleteValue(Node *root, T value) {
+    Node *target = getNodeByValue(root, value);
+    removeNode(target);
+}
+void insert(Node **head, double value)
+{
+    Node *tmp = NULL;
+    if (*head == NULL) { // іф для створення корневого елементу
+        *head = getFreeNode(value, NULL);
+        return;
+    }
+
+    tmp = *head;
+    while (tmp) {
+        if (value > tmp->data) {
+            if (tmp->right) {
+                tmp = tmp->right;
+                continue;
+            } else {
+                tmp->right = getFreeNode(value, tmp);
+                return;
+            }
+        } else if (value < tmp->data) {
+            if (tmp->left) {
+                tmp = tmp->left;
+                continue;
+            } else {
+                tmp->left = getFreeNode(value, tmp);
+                return;
+            }
+        } else {
+            exit(2);
+        }
+    }
+}
+
+void preorderPrint(Node *root) //функція обходу і виводу дерева
+{
+    if (root == NULL)
+    {
+       return;
+    }
+    printf("%.2f\n", root->data);
+    preorderPrint(root->left);
+    preorderPrint(root->right);
+   // printf("%.2f\n", root->data);
+}
+void release_node(Node *node)
+{
+    if (node)
+    {
+        if (node->parent)
+            free(node->parent);
+        free(node);
+    }
+}
+void delete_tree(Node *root) //
+{
+    if (root->left)
+        delete_tree(root->left);
+    if (root->right)
+        delete_tree(root->right);
+
+    release_node(root);
+}
