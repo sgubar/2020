8f162fc6208afb735eafc18f80f272f9b9cf5d9a
YurchenkoSerhiy <64482062+YurchenkoSerhiy@users.noreply.github.com>
2020-06-24 08:05:28 +0300

    Create dk91.h

---
+++ dk91/YurchenkoSerhiy/LAB2/dk91.h
@@ -0,0 +1,10 @@
+#ifndef SimpleSort_h
+#define SimpleSort_h
+
+int Create(int anArray[], int aSize);
+int print(int anArray[], int aSize);
+void bubbleSort(int anArray[], int aSize);
+void selectionSort(int anArray[], int aSize);
+void insertionSort(int anArray[], int aSize);
+
+#endif
