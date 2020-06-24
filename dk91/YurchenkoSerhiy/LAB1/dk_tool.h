742acda7cf7f0dfa4d677e51a6b47bba2d3a13e4
YurchenkoSerhiy <64482062+YurchenkoSerhiy@users.noreply.github.com>
2020-06-24 07:54:15 +0300

    Create dk_tool.h

---
+++ dk91/YurchenkoSerhiy/LAB1/dk_tool.h
@@ -0,0 +1,6 @@
+#ifndef DK_TOOL_H_INCLUDED
+#define DK_TOOL_H_INCLUDED
+void Factorial(int A, int *factorial);
+void Modul(int A, int B, int *ModulZnam, int *Znam);
+void Result(int factorial, int *Znam, int *ModulZnam, float *result);
+#endif // DK_TOOL_H_INCLUDED
