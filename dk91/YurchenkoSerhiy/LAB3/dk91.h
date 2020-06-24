29a606e6bb8f9538ab0dd27c0bb053a6fa977e09
YurchenkoSerhiy <64482062+YurchenkoSerhiy@users.noreply.github.com>
2020-06-24 08:12:05 +0300

    Create dk91.h

---
+++ dk91/YurchenkoSerhiy/LAB3/dk91.h
@@ -0,0 +1,9 @@
+#ifndef DK91_H_INCLUDED
+#define DK91_H_INCLUDED
+
+void quicksort(int *m,  int first, int last);
+int out(int *m, int n);
+int Create(int *m, int n);
+int poisk(int *m, int n, int key);
+
+#endif  DK91_H_INCLUDED
